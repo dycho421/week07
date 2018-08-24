9837ca5a3f3a8ec0f1ed772d80c7ffb41c96934f
lovon1000 <student@PIARTCENTER47s-iMac.local>
2018-08-24 15:47:23 -0400

    Test02

--- sketch.js
+++ sketch.js
@@ -1,65 +1,64 @@
-var canvas;
-var r,g,b;
-var moving = 0;
+var x = 50;
+var y = 200;
+var speed = 5;
 
-function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
+var sound, amplitude;
+
+function preload() {
+  sound = loadSound('https://res.cloudinary.com/doutwyts6/video/upload/v1476769079/1-01_Just_Dance_Feat._Colby_O_Donis_cmi2jz.mp3');
   
-  canvas.position (0,0);
-  canvas.style ('z-index','-1000');
-    
-  r = random(255);
-  g = random(255);
-  b = random(255);
+  sound1 = loadSound('https://res.cloudinary.com/doutwyts6/video/upload/v1476795864/90081__jivatma07__sinebd-short-w-click_y20ghx.wav');
+  
+  sound2 = loadSound('https://res.cloudinary.com/doutwyts6/video/upload/v1476795867/322758__mishicu__75-drums6-sq8l_ed0vjc.wav');
   
 }
 
-function draw() {
-  background(10,10,50);
+function setup () {
+  createCanvas(windowWidth, windowHeight);
+  sound.play();
+  amplitude = new p5.Amplitude();
+}
+
+
+function draw () {
+  background(60,60,60,50);
   
   push();
-  noFill();
-  stroke(200);
-  strokeWeight(moving*0.05);
-  rectMode(RADIUS);
-  rect(width/2,height/2,moving*0.3,moving*0.3);
-  moving = moving + 0.01;
-  pop();
-  
-  push();
-  fill(r,g,b);
+  fill(100,200,0);
   noStroke();
-  ellipse(width/2,height/2,moving*0.1,moving*0.1);
-  
-  r++;
-  g++;
-  
-  moving = moving + 1;
-  pop();
-  
+  rect(x,y,100,55,25);
   
   push();
-  noFill();
-  stroke(r,g,b);
-  strokeWeight(1);
-  // line(x1, y1, x2, y2)
-  line(width/2,height/2, 50, 450);
-  line(width/2,height/2, 350, 250);
-  pop();
+  fill('white')
+  rect(x+65,y+10,20,15);
   
   push();
-  noFill();
-  stroke(r,g,b);
-  ellipse(width/2,height/2, 500, 500);
-  rect(width/2,height/2,150,150);
-  rect(width/2,height/2,20,20);
+  fill('black');
+  ellipse(x+10,y+50,30,30);
+  ellipse(x+90,y+50,30,30);
   pop();
   
+  if (x > 700) {
+    speed = speed*-1;
+  } else if ( x < 0) {
+    speed = speed*-1;
+  }
   
-//   ellipse(250,140,20,20);
-//   ellipse(350,340,50,50);
-  
-  
-  
+  x = x + speed*2; 
+}
+
+function keyTyped() {
+  recentKey = key;
+  switch (key) {
+    case 'a':
+      sound1.play();
+      background (255,0,0);
+      break;
+    case 'l':
+      sound2.play();
+      background (0,255,0);
+      break;
+      
+  }
   
 }
